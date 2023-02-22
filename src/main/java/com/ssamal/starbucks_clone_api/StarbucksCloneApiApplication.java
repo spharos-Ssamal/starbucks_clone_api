d80d0637b5fabb3d793cf3ed59c6203d532e7790
swj9707 <swj1265@naver.com>
2023-02-22 14:31:28 +0900

    init commit

---
+++ src/main/java/com/ssamal/starbucks_clone_api/StarbucksCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.ssamal.starbucks_clone_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksCloneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssamal/starbucks_clone_api/StarbucksCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssamal.starbucks_clone_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
