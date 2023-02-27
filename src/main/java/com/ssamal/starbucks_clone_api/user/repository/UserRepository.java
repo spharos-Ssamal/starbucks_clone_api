d309db18c8cf5dbf741c98ebc2be272a481d5b72
MINJAE <wjsalswo985@naver.com>
2023-02-27 15:02:11 +0900

    Init : h2 셋팅 관련 환경설정(수정)

--- src/main/java/com/ssamal/starbucks_clone_api/user/repository/UserRepository.java
+++ src/main/java/com/ssamal/starbucks_clone_api/user/repository/UserRepository.java
@@ -3,10 +3,10 @@ package com.ssamal.starbucks_clone_api.user.repository;
 import com.ssamal.starbucks_clone_api.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
-    User findByuserNickname(String userName);
+    Optional<User> findByuserNickname(String username);
 
 }
